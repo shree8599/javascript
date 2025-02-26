9c07260cb0c50a0f6a4183b2da4e6e29b20e5dd8
shree8599 <shreemandhare5087@gmail.com>
2025-02-26 19:45:59 +0530

    dark

--- pattern.js
+++ pattern.js
@@ -1,8 +1,14 @@
 
-for(let i =1;i<=5;i++){
-    let str ="";
-    for(let j=1;j<=i*2-1;j++){
-        document.write(j+"\t");
+   // que 4
+   function printPattern(n) {
+    for (let i = n; i >= 1; i--) {
+      let spaces = " ".repeat((n - i) * 4); // Calculate leading spaces
+      let numbers = "";
+      for (let j = 1; j <= (2 * i - 1); j++) {
+        numbers += j + (j === (2 * i - 1)? "": " "); // Add space unless it's the last number
+      }
+      console.log(spaces + numbers);
     }
-    document.write("<br>");
-}
+  }
+  
+  printPattern(5);
