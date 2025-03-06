47c2a8d81784de02715d703b3bcfa86499f496d6
shree8599 <shreemandhare5087@gmail.com>
2025-03-06 17:01:51 +0530

    moviefinder

---
+++ movie/movie.js
@@ -0,0 +1,34 @@
+
+
+const form = document.querySelector("form")
+const input = document.querySelector("input")
+const apikey =  "3ca1f87f"
+const movie = document.querySelector(".movie")
+const BASE_PATH = `http://img.omdbapi.com/?apikey=${apikey}&`
+
+
+form.addEventListener("submit" , async (e)=> {
+    e.preventDefault();
+    
+
+try{
+const response = await fetch(`http://www.omdbapi.com/?t=${input.value}&apikey=${apikey}`)
+const result = await response.json()
+console.log(result)
+displayData(result)
+}catch (error){
+   console.log(error)
+}
+
+})
+
+
+function displayData(data) {
+    movie.innerHTML = "";
+    const fragment = document.createDocumentFragment();
+    const image = document.createElement("img");
+    image.src = data.Poster !== "N/A" ? data.Poster : "placeholder.jpg";
+    fragment.append(image);
+    movie.append(fragment);
+  }
+
